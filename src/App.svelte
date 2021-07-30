ac33a692ef664c8ff2d215aaf6732705a7a1467b
Russ Carroll <info@rcarroll.dev>
2021-07-30 19:59:53 -0400

    Finished app.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,29 @@
 <script>
+	import Search from './Search.svelte'
+
 	let usernameInputField = '';
+	let repositories = undefined;
+
+	async function onSubmit() {
+		const url = `https://api.github.com/users/${usernameInputField}/repos`;
+
+		const response = await fetch(url);
+		repositories = await response.json();
+		console.log('Loaded repositories', repositories)
+	}
 </script>
 
 <main>
-	<form>
+	<form on:submit|preventDefault={onSubmit}>
 		<label for="usernam">Enter a Github username:</label>
 		<input type="text" name="username" placeholder="Enter Username" bind:value={usernameInputField} />
-		<button type="submit">Load repositories</button>
+		<button type="submit" >Load repositories</button>
 	</form>
+
+	{#if repositories}
+		<Search {repositories} /> <!-- Svelte shortcut for passing prop like repositories={repositories} -->
+	{/if}
+
 </main>
 
 <style>
---
+++ src/Search.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let repositories;
+
+  let userSearchTerm = "";
+
+  $: filteredRepos = repositories.filter((repo) => {
+    return repo.name.toLowerCase().includes(userSearchTerm.toLocaleLowerCase())
+  })
+</script>
+
+<style>
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+    li {
+      padding: 10px 5px;
+    }
+    li:nth-child(odd) {
+      background-color: lightblue;
+    }
+    code {
+      display: block;
+    }
+    .search-wrapper {
+      border: 1px solid #ccc;
+      border-radius: 10px;
+      padding: 5px;
+      marging: 10px auto;
+    }
+    .search-form input {
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      width: 100%;
+    }
+</style>
+
+<div class="search-wrapper">
+  <form class="search-form">
+    <input type="text" bind:value={userSearchTerm} placeholder="Search for repositories" />
+  </form>
+  <ul>
+    {#each filteredRepos as repository}
+      <li><strong>{repository.name}</strong> <code>{repository.url}</code></li>
+    {/each}
+  </ul>
+</div>
